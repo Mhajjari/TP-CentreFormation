3603d5ce910d2a08ab323d2df883c1922648c05c
med <hajjari.mohammed@gmail.com>
2013-08-19 09:47:26 +0200

    mohammed push

--- src/main/java/fr/treeptik/pojo/Seminaire.java
+++ src/main/java/fr/treeptik/pojo/Seminaire.java
@@ -10,6 +10,7 @@ import javax.persistence.Table;
 @Table(name = "SEMINAIRE")
 public class Seminaire implements Serializable {
 
+	
 	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private Date DateSession;
